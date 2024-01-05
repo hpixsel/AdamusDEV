a8de934440c8ad9bd52d2ce1136465b861e9a44a
Kacper Adamus <hpixsel@o2.pl>
2024-01-05 16:36:46 +0100

    Init

---
+++ src/lib/components/Footer/index.svelte
@@ -0,0 +1,1 @@
+<footer>Adamus</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Navbar from "$lib/components/Navbar/index.svelte"
+  import Footer from "$lib/components/Footer/index.svelte"
+  import "../styles/style.css"
+</script>
+
+<div class="container">
+  <Navbar />
+  <div class="main">
+    <slot />
+  </div>
+  <Footer />
+</div>
