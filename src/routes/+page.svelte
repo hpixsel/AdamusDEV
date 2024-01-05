bddde52eb3d243003ebf3625d7b964e8a5c4e274
Kacper Adamus <hpixsel@o2.pl>
2024-01-05 18:32:35 +0100

    Hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  <span>documentation</span>
-</p>
+<script>
+  import Hero from "$lib/components/Hero/index.svelte"
+</script>
+
+<Hero />
